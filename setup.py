ef50830f5bf0a57add5fc63da97db02b9e9f462e
Xixuan Han <xixuanhan@gmail.com>
2018-11-22 16:49:11 +0800

    first commit

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@
 
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='FlowRisk',
     version='0.1',
     description='Order flow risk measures in Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/hanxixuana/flowrisk',
     author='Xixuan Han',
     author_email='xixuanhan@gmail.com',
